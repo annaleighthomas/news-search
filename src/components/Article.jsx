1ce981e23b39888f21ec8bf5cf55cf0f769e88d6
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-28 21:18:46 -0400

    Article Presentational Component

---
+++ src/components/Article.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+
+const Article = ({ title, author, description }) => (
+  <div>
+    <p>{title}</p>
+    <p>{author}</p>
+    <p>{description}</p>
+  </div>
+);
+
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
+
+export default Article;
