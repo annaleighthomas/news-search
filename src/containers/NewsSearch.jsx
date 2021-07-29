7976aa8a0ead736d48379336ecd45c40b0dd22fe
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-28 22:28:40 -0400

    changed articles to array and fetch to fetch articles out of object

--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -7,16 +7,18 @@ export default class NewSearch extends React.Component {
   state = {
     search: '',
     loading: true,
-    articles: ''
+    articles: []
   };
 
   async componentDidMount() {
-    const articles = await fetchArticles();
+    // const { articles } = await fetchArticles();
+    const response = await fetchArticles();
+    const articles = response.articles;
     this.setState({ articles, loading: false });
   }
 
   render() {
-    const { articles, loading } = this.state;
+    const { articles, loading } = this.state; // { articles: [], loading: false }
     if (loading) return <h1>...loading</h1>;
 
     return <ArticleList articles={articles} />;
