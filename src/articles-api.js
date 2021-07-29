2a921dc2d7109c97738065e8d2be2a79b7ee96f7
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-28 22:27:53 -0400

    added api url

--- src/articles-api.js
+++ src/articles-api.js
@@ -1,5 +1,6 @@
 export const fetchArticles = async () => {
-  const res = await fetch('https://newsapi.org/v2/everything&apiKey=API_KEY');
+  // eslint-disable-next-line max-len
+  const res = await fetch(`https://newsapi.org/v2/everything?apiKey=316703d8c9e4497e83bd8b3dc992e8ba&q=britney`);
   const json = await res.json();
   return json;
 };
